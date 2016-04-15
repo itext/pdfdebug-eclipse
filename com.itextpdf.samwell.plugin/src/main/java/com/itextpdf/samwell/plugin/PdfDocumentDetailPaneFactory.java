9c935273cd812adb19f8d6e27ff20f1523f7cd5a
Ilya Idamkin <ilya.idamkin@duallab.com>
2016-04-15 10:15:33 +0300

    Various changes to DetailPaneFactory needed for implementing license checks
    DEVSIX-511

--- com.itextpdf.samwell.plugin/src/main/java/com/itextpdf/samwell/plugin/PdfDocumentDetailPaneFactory.java
+++ com.itextpdf.samwell.plugin/src/main/java/com/itextpdf/samwell/plugin/PdfDocumentDetailPaneFactory.java
@@ -1,5 +1,7 @@
 package com.itextpdf.samwell.plugin;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,26 +11,39 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 
 public class PdfDocumentDetailPaneFactory implements IDetailPaneFactory {
 
-    protected static final HashSet<String> ids = new HashSet<String>();
-    protected static final HashSet<String> empty = new HashSet<String>();
+    protected static final HashSet<String> ids = new HashSet<>();
+    protected static final HashMap<String, String> names = new HashMap<>();
+    protected static final HashMap<String, String> descriptions = new HashMap<>();
+    protected static final HashMap<String, Class<? extends IDetailPane> > classes = new HashMap<>();
 
     static {
         ids.add(RupsDetailPane.ID);
+        names.put(RupsDetailPane.ID, RupsDetailPane.NAME);
+        descriptions.put(RupsDetailPane.ID, RupsDetailPane.DESCRIPTION);
+        classes.put(RupsDetailPane.ID, RupsDetailPane.class);
     }
 
     @Override
     public IDetailPane createDetailPane(String paneID) {
-        return new RupsDetailPane();
+    	Class<? extends IDetailPane> paneClass = classes.get(paneID);
+    	if (paneClass != null) {
+    		try {
+				return paneClass.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				return null;
+			}
+    	}
+    	return null;
     }
 
     @Override
     public String getDetailPaneName(String paneID) {
-        return RupsDetailPane.NAME;
+        return names.get(paneID);
     }
 
     @Override
     public String getDetailPaneDescription(String paneID) {
-        return RupsDetailPane.DESCRIPTION;
+        return descriptions.get(paneID);
     }
 
     @Override
@@ -36,11 +51,14 @@ public class PdfDocumentDetailPaneFactory implements IDetailPaneFactory {
         if (RupsDetailPane.isPdfDocument(selection)) {
             return ids;
         }
-        return empty;
+        return Collections.emptySet();
     }
 
     @Override
     public String getDefaultDetailPane(IStructuredSelection selection) {
-        return RupsDetailPane.ID;
+        if (RupsDetailPane.isPdfDocument(selection)) {
+        	return RupsDetailPane.ID;
+        }
+        return null;
     }
 }
